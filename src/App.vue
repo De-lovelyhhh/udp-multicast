2454f2ad4eced714626c10e64bae511c549f15c5
xv1998 <1324841784@qq.com>
2020-09-23 22:24:17 +0800

    feat: init

--- src/App.vue
+++ src/App.vue
@@ -9,17 +9,16 @@
       </p>
       <el-button>el-button</el-button>
     </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CheckBox from './components/CheckBox.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    CheckBox
   }
 }
 </script>
